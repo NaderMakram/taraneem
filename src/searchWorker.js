ec841dff08830260eb2457b53af011de5f881860
NaderMakram <nadermakram98@gmail.com>
2025-04-06 17:37:39 +0200

    fix few songs, replace order of results to have songs before verses

--- src/searchWorker.js
+++ src/searchWorker.js
@@ -17,7 +17,7 @@ function performSearch(term, songsWithSearchableContent, bibleVerses) {
   console.log(`Actual search time: ${searchTime.toFixed(2)} ms`);
   // console.log(`songResults: ${songResults}`);
   // console.log(`bibleResults: ${bibleResults}`);
-  return [...bibleResults, ...songResults];
+  return [...songResults, ...bibleResults];
 }
 
 function performChapterSearch(term, bibleChapters) {
